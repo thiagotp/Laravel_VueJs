232a1a96fc18232b4078dd1cee3aadeca7ef5fcd
thiagotp <thiagoteixeira.ps@gmail.com>
2022-08-02 23:50:25 -0300

    usando tinker

--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    
+    protected $table = 'site_contatos';
+    protected $fillable = ['name', 'telefone', 'motivo_contato', 'email', 'mensagem'];
 }
